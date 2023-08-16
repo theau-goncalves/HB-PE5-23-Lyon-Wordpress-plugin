99f1d0625478201ced410e7ea326a34f29d9de30
Goncalves Théau <theau26@gmail.com>
2023-08-16 11:04:37 +0200

    Add composer configuration

--- hb-post-generator.php
+++ hb-post-generator.php
@@ -1,1 +1,13 @@
 <?php
+/*
+ * Plugin Name:       Human Booster post generator
+ * Description:       Generate fake posts
+ * Version:           1.0.0
+ * Requires at least: 6.0
+ * Requires PHP:      8.0
+ * Author:            Théau Goncalves | Drosalys
+ * Author URI:        https://drosalys.fr/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       hb-post-generator
+ */
